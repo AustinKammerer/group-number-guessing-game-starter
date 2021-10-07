ba7a8ea3b5dee4213f15fcd9962ace534357388d
Austin Kammerer <austin4191@gmail.com>
2021-10-07 13:27:30 -0500

    randNum function, initialized guesses array

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5000;
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve up static files (HTML, CSS, Client JS)
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
 
 // GET & POST Routes go here
 
-
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+  console.log("Server is running on port", PORT);
+});
+
+function randNum(min, max) {
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+let guesses = [];
+
+let winningNum = randNum(1, 25);
